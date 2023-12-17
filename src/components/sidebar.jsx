5664f2e3ad7d384c8833f53b196946d6b2d5dafc
RanditZX <daivaraditya36@gmail.com>
2023-12-17 20:01:16 +0700

    About page

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,23 @@
+import '../assets/css/sidebar.css'
+
+export const Sidebar = () => {
+    return(
+        <div className="container shadow-black flex">
+            <div className="title">
+                <h1>The Jamu’s</h1>
+            </div>
+            <div className="content">
+                <div className="content-1">
+                    <p>About</p>
+                </div>
+                <div className="content-1">
+                    <p>History</p>
+                </div>
+                <div className="content-1">
+                    <p>Recipes</p>
+                </div>
+            </div>
+           
+        </div>
+    )
+}
