ff3694c1e9997932436c64fac67b759a773fb4a8
marudever <marudever@gmail.com>
2023-12-20 10:19:00 +0700

    FEAT / Landing and Transition Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { AboutJamus } from "./modules/aboutJamus";
+import { JamuTypes } from "./modules/jamuTypes";
+import { Landing } from "./modules/landing";
+import { StepByStep } from "./modules/stepByStep";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={Landing()} />
         <Route path="/about-jamus" element={AboutJamus()} />
+        <Route path="/step-by-step" element={StepByStep()} />
+        <Route path="/jamu-types" element={JamuTypes()} />
       </Routes>
     </BrowserRouter>
   );
