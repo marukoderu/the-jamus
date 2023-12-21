7fb4e611cd436fbb74f28303f8ad0cd4d718b18d
dirarmdn <dhiraramadini@gmail.com>
2023-12-21 23:26:16 +0700

    (FIX) responsive bismillah jadi, animasi seadanya insyaAllah tetap berkah

--- src/modules/jamuTypes.jsx
+++ src/modules/jamuTypes.jsx
@@ -1,27 +1,26 @@
 import "../assets/css/jamu-types.css";
-import { gsap } from "gsap";
 
 import { useEffect } from "react";
 import { Sidebar } from "../components/sidebar";
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export const JamuTypes = () => {
+
+  
   useEffect(() => {
-    const verticalScrollDistance = 300; // Typical vertical scroll distance
-    const horizontalScrollDistance = verticalScrollDistance; // Adjust to match vertical scroll speed
+    AOS.init();
 
     const handleScroll = (event) => {
       event.preventDefault();
-      if (event.deltaY !== 0) {
-        const scrollAmount =
-          event.deltaY > 0
-            ? horizontalScrollDistance
-            : -horizontalScrollDistance;
-        window.scrollBy({
-          left: scrollAmount,
-          behavior: "smooth", // Add smooth scrolling if desired
-        });
-      }
+
+      const horizontalScrollDistance = 400;
+      const scrollAmount = event.deltaY > 0 ? horizontalScrollDistance  : -horizontalScrollDistance;
+
+      window.scrollBy({
+        left: scrollAmount,
+        behavior: "smooth",
+      });
     };
 
     window.addEventListener("wheel", handleScroll, { passive: false });
@@ -29,80 +28,56 @@ export const JamuTypes = () => {
     return () => {
       window.removeEventListener("wheel", handleScroll);
     };
-
   }, []);
 
-  useEffect(() => {
-  let tlodd = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".types-title",
-      start: "70% 80%",
-      end: () => "top bottom",
-      toggleActions: "restart pause resume none",
-    },
-  });
 
-  tlodd.to([".title-step h1", ".title-step p"], {
-    y: -270,
-    duration: 5,
-  });
-
-  tlodd.to(
-    ".jamu-types img",
-    {
-      y: 20,
-      duration: 3,
-    },
-    0
-  );
-});
 
 const Divider = () => (
-  <div className="divider lg:py-8 p-4">
-    <div className="lg:border-r-2 lg:border-r-primary lg:h-full border-b-2 border-b-primary w-full"></div>
+  <div className="divider md:py-8 p-4">
+    <div className="md:border-r-2 md:border-r-primary md:h-full border-b-2 border-b-primary w-full"></div>
   </div>
 );
 
-  function JamusTypes1() {
-    return (
-        <div className="jamu-types text-primary">
-            <div className="flex lg:flex-col flex-col-reverse px-12 lg:pt-20 pt-5 lg:-mb-16 gap-12">
-                <h1 className="font-playfair text-6xl font-medium">BERAS<br /> KENCUR</h1>
-                <p className="font-neue font-normal mb-16 mt-10 w-3/4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
-                <img src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
-            </div>
-        </div>
-    )
-  }
-
-  function JamusTypes2() {
-    return (
-        <div className="jamu-types text-primary">
-            <div className="flex flex-col px-12 lg:-mt-64 gap-12">
-                <img src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
-                <p className="font-neue font-normal mt-16 mb-10 w-3/4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
-                <h1 className="font-playfair text-6xl font-medium">BERAS<br /> KENCUR</h1>
-            </div>
-        </div>
-    )
-  }
-
   return (
     <>
+      <div id="scrollContainer" className="container-types flex md:flex-row flex-col w-full">
       <Sidebar />
-      <div id="scrollContainer" className="container-types flex lg:flex-row flex-col">
-        <div className="types-title font-playfair text-primary py-1 flex flex-nowrap lg:ml-36">
-            <h1 className="text-9xl pr-3 pl-4 font-medium">DIVE INTO JAMU</h1>
+        <div className="types-title font-playfair text-primary py-1 flex flex-nowrap md:ml-36 md:ml-32 ml- md:pt-20">
+            <h1 className="text-9xl pr-3 pl-4 text-center font-medium">DIVE INTO JAMU</h1>
         </div>
         <Divider />
-        <div className="flex lg:flex-row flex-col">
-            <JamusTypes1 />
+        <div className="flex md:flex-row flex-col w-fit">
+          <a href="#" className="jamu-types text-primary">
+              <div className="flex md:flex-col flex-col-reverse px-12 md:pt-20 pt-5 md:-mb-16 gap-12">
+                  <h1 data-aos="fade-down" data-aos-duration="1500" className="font-playfair text-6xl font-medium">BERAS<br /> KENCUR</h1>
+                  <p className="font-neue font-normal text-justify md:text-2xl text-xl mb-16 mt-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+                  <img data-aos="fade-up" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
+              </div>
+          </a>
             <Divider />
-            <JamusTypes2 />
+            <a href="#" className="jamu-types text-primary">
+            <div className="flex flex-col px-12 md:-mt-64 gap-12">
+                <img data-aos="fade-down" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
+                <p className="font-neue font-normal text-justify md:text-2xl text-xl md:mt-16 md:mb-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+                <h1 data-aos="fade-up" data-aos-duration="1500" className="font-playfair text-6xl font-medium">BERAS<br /> KENCUR</h1>
+            </div>
+            </a>
             <Divider />
-            <JamusTypes1 />
+            <a href="#" className="jamu-types text-primary">
+              <div className="flex md:flex-col flex-col-reverse px-12 md:pt-20 pt-5 md:-mb-16 gap-12">
+                  <h1 data-aos="fade-down" data-aos-duration="1500" className="font-playfair text-6xl font-medium">BERAS<br /> KENCUR</h1>
+                  <p className="font-neue font-normal text-justify md:text-2xl text-xl mb-16 mt-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+                  <img data-aos="fade-up" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
+              </div>
+           </a>
             <Divider />
-            <JamusTypes2 />
+            <a href="#" className="jamu-types text-primary">
+              <div className="flex flex-col px-12 md:-mt-64 gap-12">
+                  <img data-aos="fade-down" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
+                  <p className="font-neue font-normal text-justify md:text-2xl text-xl md:mt-16 md:mb-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+                  <h1 data-aos="fade-up" data-aos-duration="1500" className="font-playfair text-6xl font-medium">BERAS<br /> KENCUR</h1>
+              </div>
+            </a>
             <Divider />
         </div>
       </div>
--- src/modules/stepByStep.jsx
+++ src/modules/stepByStep.jsx
@@ -2,90 +2,97 @@ import "../assets/css/step-by-step.css";
 
 import { useEffect } from "react";
 import { Sidebar } from "../components/sidebar";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export const StepByStep = () => {
 
 	useEffect(() => {
-		const verticalScrollDistance = 300; // Typical vertical scroll distance
-		const horizontalScrollDistance = verticalScrollDistance; // Adjust to match vertical scroll speed
-	
-		const handleScroll = (event) => {
-		  event.preventDefault();
-		  if (event.deltaY !== 0) {
-			const scrollAmount =
-			  event.deltaY > 0
-				? horizontalScrollDistance
-				: -horizontalScrollDistance;
-			window.scrollBy({
-			  left: scrollAmount,
-			  behavior: "smooth", // Add smooth scrolling if desired
-			});
-		  }
-		};
-	
-		window.addEventListener("wheel", handleScroll, { passive: false });
-	
-		return () => {
-		  window.removeEventListener("wheel", handleScroll);
-		};
-	});
+        AOS.init();
+        AOS.refresh();
+        
+        const handleScroll = (event) => {
+            event.preventDefault();
+      
+            const horizontalScrollDistance = 400;
+            const scrollAmount = event.deltaY > 0 ? horizontalScrollDistance  : -horizontalScrollDistance;
+    
+            window.scrollBy({
+              left: scrollAmount,
+              behavior: "smooth",
+            });
+          };
+      
+          window.addEventListener("wheel", handleScroll, { passive: false });
+      
+          return () => {
+            window.removeEventListener("wheel", handleScroll);
+          };
+        }, []);
   
   const Divider = () => (
-    <div className="divider lg:py-8 p-4">
-      <div className="lg:border-r-2 lg:border-r-white lg:h-full border-b-2 border-b-white w-full"></div>
+    <div className="divider md:py-8 p-4">
+      <div className="md:border-r-2 md:border-r-white md:h-full border-b-2 border-b-white w-full"></div>
     </div>
   );
 
   return (
     <>
-      <div id="scrollContainer" className="container-steps bg-primary">
-        <Sidebar />
-        <div className="flex justify-center items-center bg-primary pt-10">
-          <div className="flex flex-col lg:pl-20 lg:ml-10 md:pr-72 pr-0 sm:px-auto pt-14 gap-8 justify-center items-center text-white font-playfair">
-            <p className="font-neue font-normal lg:text-2xl text-lg">A step-by-step how to make jamu</p>
-            <h1 className="lg:text-9xl text-7xl">BERAS</h1>
-            <h1 className="lg:text-9xl text-7xl lg:pl-64 md:pl-64 -mt-6">KENCUR</h1>
-            <p className="font-neue font-bold lg:text-2xl text-lg tracking-wider mt-8">Scroll <span className="ornament"></span></p>
+      <div id="scrollContainer" className="container-steps bg-primary flex md:flex-row flex-col">
+      <Sidebar />
+        <div className="title-steps flex justify-center items-center bg-primary pt-10 lg:w-1/5 w-full">
+          <div className="flex flex-col md:pl-20 md:ml-10 md:pr-72 pr-0 sm:px-auto pt-14 gap-8 justify-center items-center text-white font-playfair">
+            <p data-aos="fade-right" data-aos-duration="1000" className="font-neue font-normal lg:text-2xl text-lg">A step<span className="font-playfair">-</span>by<span className="font-playfair">-</span>  step how to make jamu</p>
+            <h1 data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300" className="lg:text-9xl text-7xl">BERAS</h1>
+            <h1 data-aos="fade-up" data-aos-duration="1500" data-aos-delay="500" className="lg:text-9xl text-7xl lg:pl-64 md:pl-64 -mt-6">KENCUR</h1>
+            <p data-aos="fade-left" data-aos-duration="1000" className="font-neue font-bold lg:text-2xl text-lg tracking-wider mt-8">Scroll <span className="ornament"></span></p>
           </div>
         </div>
-		<Divider />
-          <div className="flex lg:flex-row flex-col lg:mt-0 bg-primary text-white font-neue">
-            <div className="flex justify-center items-center lg:text-9xl text-7xl lg:ml-24 my-5 font-playfair">
-              <h1 className="number">1</h1>
-            </div>
-            <div className="jamu-types flex text-primary">
-              <div className="flex flex-col justify-center items-center">
-                <img src={require("../assets/img/jamus-1.png")} alt="" className="img-step-odd rounded-xl w-3/4 lg:mb-0 mb-5 order-1" />
-                <p className="font-neue text-odd lg:translate-y-[-200%] lg:-mt-60 lg tracking-widest font-light text-justify lg:text-2xl md:text-3xl text-xl lg:w-3/4 lg:my-auto my-15 px-10 text-white sm:translate-y-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+		    <Divider />
+        
+        <div className="flex md:flex-row flex-col w-full text-white">
+          <div className="jamu-steps mx-auto flex md:flex-row flex-col">
+              <div className="flex justify-center items-center lg:text-9xl text-7xl font-playfair">
+                <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300" className="number md:ml-5 md:mr-3 ml-0 mb-5">1</h1>
+              </div>
+              <div className="flex flex-col px-12 lg:-mt-64 gap-12">
+                <img data-aos="fade-down" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
+                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl lg:mt-16 lg:mb-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
               </div>
             </div>
-			</div>
-          <Divider />
-            <div className="flex lg:flex-row flex-col lg:mt-0 bg-primary text-white font-neue">
-              <div className="flex justify-center items-center lg:text-9xl text-7xl lg:ml-24 my-5 font-playfair">
-                <h1 className="number">2</h1>
+            <Divider />
+          <div className="jamu-steps mx-auto flex md:flex-row flex-col">
+                <div className="flex justify-center items-center lg:text-9xl text-7xl font-playfair">
+                    <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300" className="number md:ml-5 md:mr-3 ml-0 mb-5">2</h1>
+                </div>
+              <div className="flex md:flex-col flex-col-reverse px-12 lg:pt-20 pt-5 lg:-mb-16 gap-12">
+                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl md:mb-16 md:mt-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+                  <img data-aos="fade-up" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
               </div>
-              <div className="jamu-types flex text-primary">
-                <div className="flex flex-col lg:-mb-20 justify-center items-center">
-                  <img src={require("../assets/img/jamus-1.png")} alt="" className="img-step-even rounded-xl w-3/4 lg:mb-0 mb-5" />
-				  <p className="font-neue text-odd lg:translate-y-[-200%] lg:-mt-60 lg tracking-widest font-light text-justify lg:text-2xl md:text-3xl text-xl lg:w-3/4 lg:my-auto my-15 p-10 text-white sm:translate-y-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
-				</div>
+          </div>
+            <Divider />
+            <div className="jamu-steps mx-auto flex md:flex-row flex-col">
+              <div className="flex justify-center items-center lg:text-9xl text-7xl font-playfair">
+                    <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300" className="number md:ml-5 md:mr-3 ml-0 mb-5">3</h1>
               </div>
-            </div>
-          <Divider />
-          <div className="flex lg:flex-row flex-col lg:mt-0 bg-primary text-white font-neue">
-            <div className="flex justify-center items-center lg:text-9xl text-7xl lg:ml-24 my-5 font-playfair">
-              <h1 className="number">3</h1>
-            </div>
-            <div className="jamu-types flex text-primary">
-              <div className="flex flex-col justify-center items-center">
-                <img src={require("../assets/img/jamus-1.png")} alt="" className="img-step-odd rounded-xl w-3/4 lg:mb-0 mb-5 order-1" />
-                <p className="font-neue text-odd lg:translate-y-[-200%] lg:-mt-60 lg tracking-widest font-light text-justify lg:text-2xl md:text-3xl text-xl lg:w-3/4 lg:my-auto my-15 px-10 text-white sm:translate-y-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+              <div className="flex flex-col px-12 lg:-mt-64 gap-12">
+                  <img data-aos="fade-down" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
+                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl lg:mt-16 lg:mb-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
               </div>
             </div>
-			</div>
-          <Divider />
+            <Divider />
+            <div className="jamu-steps mx-auto flex md:flex-row flex-col">
+              <div className="flex justify-center items-center lg:text-9xl text-7xl font-playfair">
+                    <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300" className="number md:ml-5 md:mr-3 ml-0 mb-5">4</h1>
+              </div>
+              <div className="flex md:flex-col flex-col-reverse px-12 lg:pt-20 pt-5 lg:-mb-16 gap-12">
+                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl md:mb-16 md:mt-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+                  <img data-aos="fade-up" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
+              </div>
+           </div>
+            <Divider />
         </div>
+      </div>
     </>
   );
   
