bc7f6544646ec8d434d42524bbcb7b364b8d2dd8
dirarmdn <dhiraramadini@gmail.com>
2023-12-21 11:21:40 +0700

    (FIX) responsive //broken

--- src/modules/jamuTypes.jsx
+++ src/modules/jamuTypes.jsx
@@ -57,16 +57,16 @@ export const JamuTypes = () => {
   );
 });
 
-  const Divider = () => (
-    <div className="xl:py-8 px-8 my-10">
-      <div className="xl:border-r-2 xl:border-r-primary xl:h-full border-b-2 border-b-primary w-full"></div>
-    </div>
-  );
+const Divider = () => (
+  <div className="divider lg:py-8 p-4">
+    <div className="lg:border-r-2 lg:border-r-primary lg:h-full border-b-2 border-b-primary w-full"></div>
+  </div>
+);
 
   function JamusTypes1() {
     return (
         <div className="jamu-types text-primary">
-            <div className="flex xl:flex-col flex-col-reverse px-12 xl:pt-20 pt-5 xl:-mb-16 gap-12">
+            <div className="flex lg:flex-col flex-col-reverse px-12 lg:pt-20 pt-5 lg:-mb-16 gap-12">
                 <h1 className="font-playfair text-6xl font-medium">BERAS<br /> KENCUR</h1>
                 <p className="font-neue font-normal mb-16 mt-10 w-3/4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
                 <img src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
@@ -78,7 +78,7 @@ export const JamuTypes = () => {
   function JamusTypes2() {
     return (
         <div className="jamu-types text-primary">
-            <div className="flex flex-col px-12 xl:-mt-64 gap-12">
+            <div className="flex flex-col px-12 lg:-mt-64 gap-12">
                 <img src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
                 <p className="font-neue font-normal mt-16 mb-10 w-3/4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
                 <h1 className="font-playfair text-6xl font-medium">BERAS<br /> KENCUR</h1>
@@ -89,14 +89,13 @@ export const JamuTypes = () => {
 
   return (
     <>
-
-      <div id="scrollContainer" className="main_container">
       <Sidebar />
-        <div className="types-title font-playfair text-primary py-1 flex flex-nowrap ml-36">
+      <div id="scrollContainer" className="container-types flex lg:flex-row flex-col">
+        <div className="types-title font-playfair text-primary py-1 flex flex-nowrap lg:ml-36">
             <h1 className="text-9xl pr-3 pl-4 font-medium">DIVE INTO JAMU</h1>
         </div>
         <Divider />
-        <div className="flex xl:flex-row flex-col">
+        <div className="flex lg:flex-row flex-col">
             <JamusTypes1 />
             <Divider />
             <JamusTypes2 />
--- src/modules/stepByStep.jsx
+++ src/modules/stepByStep.jsx
@@ -1,11 +1,7 @@
 import "../assets/css/step-by-step.css";
 
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect } from "react";
 import { Sidebar } from "../components/sidebar";
-
-gsap.registerPlugin(ScrollTrigger);
 
 export const StepByStep = () => {
 
@@ -32,132 +28,64 @@ export const StepByStep = () => {
 		return () => {
 		  window.removeEventListener("wheel", handleScroll);
 		};
-	  }, []);
-
-    useEffect(() => {
-
-      ScrollTrigger.defaults({
-        scroller: "body"
-      });
-  
-      gsap.fromTo(
-        [".title-step h1", ".title-step p"],
-        {
-          opacity: 0,
-        },
-        {
-          y: 0,
-          opacity: 1,
-          duration: 5,
-          ease: 'power2.out',
-        }
-      );
-
-      let tlo = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".img-step-odd",
-          start: "70% 80%",
-          end: () => "+=" + document.querySelector(".img-step-odd").offsetWidth,
-          toggleActions: "restart pause resume none",
-        },
-      });
-    
-      tlo.to(".img-step-odd", {
-        y: -170,
-        duration: 3,
-      });
-
-      tlo.to(
-        ".text-odd",
-        {
-          y: 250,
-          duration: 3,
-        },
-        0
-      );
-
-      let tle = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".img-step-even",
-          start: "70% 80%",
-          end: () => "+=" + document.querySelector(".img-step-even").offsetWidth,
-          toggleActions: "restart pause resume none",
-        },
-      });
-
-      tle.to(".img-step-even", {
-        y: 380,
-        duration: 3,
-      });
-
-      tle.to(
-        ".text-even",
-        {
-          y: -100,
-          duration: 3,
-        },
-        0
-      );
-
-      let tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".number",
-          start: "70% 80%",
-          end: () => "+=" + document.querySelector(".number").offsetWidth,
-          toggleActions: "restart pause resume none",
-        },
-      });
-
-      tl.to(".number", {
-        y: -100,
-        duration: 5,
-      });
-    }, []);
-  
+	});
   
   const Divider = () => (
-    <div className="divider lg:py-8 p-8">
+    <div className="divider lg:py-8 p-4">
       <div className="lg:border-r-2 lg:border-r-white lg:h-full border-b-2 border-b-white w-full"></div>
     </div>
   );
-
-  const Step = ({ number, content, imageSrc, text, image, margin, marginTop }) => (
-    <div className="flex lg:flex-row flex-col lg:mt-0 relative">
-      <div className={`flex justify-center items-center lg:text-9xl text-7xl lg:ml-24 my-5 font-playfair`}>
-        <h1 className="number">{number}</h1>
-      </div>
-      <div className={`jamu-types flex ${margin ? `${margin}` : ""} text-primary relative z-10`}>
-        <div className={`flex flex-col ${marginTop ? `${marginTop}` : ""} justify-center items-center lg:relative z-10`}>
-          <img src={imageSrc} alt="" className={`${image ? `${image}` : ""} rounded-xl w-3/4 lg:mb-0 mb-5 ${number % 2 === 0 ? 'order-1' : ''} z-20`} />
-          <p className={`font-neue ${text ? `${text}` : ""} lg:relative tracking-widest font-light text-justify lg:text-2xl md:text-3xl text-xl lg:w-3/4 lg:my-auto my-15 w-full px-10 text-white z-10 sm:translate-y-0`}>{content}</p>
-        </div>
-      </div>
-    </div>
-  );
-  
 
   return (
     <>
       <div id="scrollContainer" className="container-steps bg-primary">
         <Sidebar />
-        <div className="title-step flex bg-primary py-10">
-          <div className="flex flex-col lg:pl-20 pl-10 pr-72 gap-8 justify-center text-white font-playfair border-r-white border-r-2">
+        <div className="flex justify-center items-center bg-primary pt-10">
+          <div className="flex flex-col lg:pl-20 lg:ml-10 md:pr-72 pr-0 sm:px-auto pt-14 gap-8 justify-center items-center text-white font-playfair">
             <p className="font-neue font-normal lg:text-2xl text-lg">A step-by-step how to make jamu</p>
             <h1 className="lg:text-9xl text-7xl">BERAS</h1>
-            <h1 className="lg:text-9xl text-7xl lg:ml-40 -mt-6">KENCUR</h1>
-            <p className="font-neue font-bold lg:text-2xl md:text-3xl text-lg tracking-wider relative mt-8">Scroll <span className="ornament"></span></p>
+            <h1 className="lg:text-9xl text-7xl lg:pl-64 md:pl-64 -mt-6">KENCUR</h1>
+            <p className="font-neue font-bold lg:text-2xl text-lg tracking-wider mt-8">Scroll <span className="ornament"></span></p>
           </div>
         </div>
-
-        <div className="flex xl:flex-row flex-col bg-primary text-white font-neue">
-          <Step number={1} content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis." imageSrc={require("../assets/img/jamus-1.png")} image={'img-step-odd'} text={'text-odd lg:translate-y-[-200%] lg:-mt-60'} />
+		<Divider />
+          <div className="flex lg:flex-row flex-col lg:mt-0 bg-primary text-white font-neue">
+            <div className="flex justify-center items-center lg:text-9xl text-7xl lg:ml-24 my-5 font-playfair">
+              <h1 className="number">1</h1>
+            </div>
+            <div className="jamu-types flex text-primary">
+              <div className="flex flex-col justify-center items-center">
+                <img src={require("../assets/img/jamus-1.png")} alt="" className="img-step-odd rounded-xl w-3/4 lg:mb-0 mb-5 order-1" />
+                <p className="font-neue text-odd lg:translate-y-[-200%] lg:-mt-60 lg tracking-widest font-light text-justify lg:text-2xl md:text-3xl text-xl lg:w-3/4 lg:my-auto my-15 px-10 text-white sm:translate-y-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+              </div>
+            </div>
+			</div>
           <Divider />
-          <Step number={2} content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis." imageSrc={require("../assets/img/jamus-1.png")} marginTop={'lg:-mb-20'} image={'img-step-even'} text={'text-even lg:-mb-60'} />
+            <div className="flex lg:flex-row flex-col lg:mt-0 bg-primary text-white font-neue">
+              <div className="flex justify-center items-center lg:text-9xl text-7xl lg:ml-24 my-5 font-playfair">
+                <h1 className="number">2</h1>
+              </div>
+              <div className="jamu-types flex text-primary">
+                <div className="flex flex-col lg:-mb-20 justify-center items-center">
+                  <img src={require("../assets/img/jamus-1.png")} alt="" className="img-step-even rounded-xl w-3/4 lg:mb-0 mb-5" />
+				  <p className="font-neue text-odd lg:translate-y-[-200%] lg:-mt-60 lg tracking-widest font-light text-justify lg:text-2xl md:text-3xl text-xl lg:w-3/4 lg:my-auto my-15 p-10 text-white sm:translate-y-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+				</div>
+              </div>
+            </div>
           <Divider />
-          <Step number={3} content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis." imageSrc={require("../assets/img/jamus-1.png")} image={'img-step-odd'} text={'text-odd lg:translate-y-[-200%] lg:-mt-60'}/>
+          <div className="flex lg:flex-row flex-col lg:mt-0 bg-primary text-white font-neue">
+            <div className="flex justify-center items-center lg:text-9xl text-7xl lg:ml-24 my-5 font-playfair">
+              <h1 className="number">3</h1>
+            </div>
+            <div className="jamu-types flex text-primary">
+              <div className="flex flex-col justify-center items-center">
+                <img src={require("../assets/img/jamus-1.png")} alt="" className="img-step-odd rounded-xl w-3/4 lg:mb-0 mb-5 order-1" />
+                <p className="font-neue text-odd lg:translate-y-[-200%] lg:-mt-60 lg tracking-widest font-light text-justify lg:text-2xl md:text-3xl text-xl lg:w-3/4 lg:my-auto my-15 px-10 text-white sm:translate-y-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+              </div>
+            </div>
+			</div>
           <Divider />
         </div>
-      </div>
     </>
   );
   
