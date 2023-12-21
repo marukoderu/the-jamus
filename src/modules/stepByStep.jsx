9bc8e92da8db9e81bef2990cd4b0ccb7896198db
dirarmdn <dhiraramadini@gmail.com>
2023-12-21 23:53:25 +0700

    (FEAT) test content

--- src/modules/stepByStep.jsx
+++ src/modules/stepByStep.jsx
@@ -51,12 +51,15 @@ export const StepByStep = () => {
 		    <Divider />
         
         <div className="flex md:flex-row flex-col w-full text-white">
-        <div className="jamu-steps mx-auto flex flex-col font-playfair justify-center px-10">
+        <div className="jamu-ingre mx-auto flex flex-col font-playfair justify-center px-10 md:my-0 my-8">
               <h1 data-aos="fade-right" data-aos-duration="1000" className="text-5xl">INGREDIENTS</h1>
-              <ol data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300" className="list-decimal mt-8 text-4xl text-left md:px-20">
-                <li>hehe</li>
-                <li>haha</li>
-                <li>hihi</li>
+              <ol data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300" className="list-decimal mt-8 md:text-3xl text-xl text-left md:px-20">
+                <li>1500 ml air</li>
+                <li>150 gram gula jawa</li>
+                <li>125 gram kencur</li>
+                <li>50 gram beras putih</li>
+                <li>5 sdm gula pasir</li>
+                <li>5 ruas jahe</li>
               </ol>
             </div>
             <Divider />
@@ -66,7 +69,7 @@ export const StepByStep = () => {
               </div>
               <div className="flex flex-col px-12 lg:-mt-64 gap-12">
                 <img data-aos="fade-down" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
-                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl lg:mt-16 lg:mb-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl lg:mt-16 lg:mb-10 lg:w-3/4 w-full">Cuci bersih beras, kemudian rendam dalam air selama 1 jam. Setelah itu, sisihkan.</p>
               </div>
             </div>
             <Divider />
@@ -75,7 +78,7 @@ export const StepByStep = () => {
                     <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300" className="number md:ml-5 md:mr-3 ml-0 mb-5">2</h1>
                 </div>
               <div className="flex md:flex-col flex-col-reverse px-12 lg:pt-20 pt-5 lg:-mb-16 gap-12">
-                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl md:mb-16 md:mt-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl md:mb-16 md:mt-10 lg:w-3/4 w-full">Rebus asam jawa, gula pasir, gula merah, dan jahe dengan air sampai mendidih. Setelah itu tunggu sampai agak dingin. Saring airnya. Sisihkan.</p>
                   <img data-aos="fade-up" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
               </div>
           </div>
@@ -86,7 +89,7 @@ export const StepByStep = () => {
               </div>
               <div className="flex flex-col px-12 lg:-mt-64 gap-12">
                   <img data-aos="fade-down" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
-                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl lg:mt-16 lg:mb-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl lg:mt-16 lg:mb-10 lg:w-3/4 w-full">Cuci bersih kencur yang masih segar. Kupas kulitnya dan potong-potong.</p>
               </div>
             </div>
             <Divider />
@@ -95,10 +98,20 @@ export const StepByStep = () => {
                     <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300" className="number md:ml-5 md:mr-3 ml-0 mb-5">4</h1>
               </div>
               <div className="flex md:flex-col flex-col-reverse px-12 lg:pt-20 pt-5 lg:-mb-16 gap-12">
-                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl md:mb-16 md:mt-10 lg:w-3/4 w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente expedita quia animi doloremque? Ducimus, facilis.</p>
+                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl md:mb-16 md:mt-10 lg:w-3/4 w-full">Tiriskan air rendaman beras. Blender beras, kencur, dan air rebusan gula hingga halus.</p>
                   <img data-aos="fade-up" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
               </div>
            </div>
+            <Divider />
+            <div className="jamu-steps mx-auto flex md:flex-row flex-col">
+              <div className="flex justify-center items-center lg:text-9xl text-7xl font-playfair">
+                    <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300" className="number md:ml-5 md:mr-3 ml-0 mb-5">5</h1>
+              </div>
+              <div className="flex flex-col px-12 lg:-mt-64 gap-12">
+                  <img data-aos="fade-down" data-aos-duration="1500" src={require("../assets/img/jamus-1.png")} alt="" className="rounded-xl mb-0" />
+                  <p className="font-neue font-normal text-justify lg:text-2xl text-xl lg:mt-16 lg:mb-10 lg:w-3/4 w-full">Saring jamu beras kencur. Peras ampas blenderan sampai benar-benar kering. Sajikan jamu beras kencur.</p>
+              </div>
+            </div>
             <Divider />
         </div>
       </div>
